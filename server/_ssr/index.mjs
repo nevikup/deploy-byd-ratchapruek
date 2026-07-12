01e6c115597998bb1a4d2f57a4cce614107d0ca0
DESKTOP-J77BNUC\Admin <nevikup@gmail.com>
2026-07-12 21:33:18 +0700

    deploy anti spam

--- server/_ssr/index.mjs
+++ server/_ssr/index.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 let serverEntryPromise;
 async function getServerEntry() {
   if (!serverEntryPromise) {
-    serverEntryPromise = import("./server-cjM90p06.mjs").then((n) => n.s).then(
+    serverEntryPromise = import("./server-BdZ_lyIR.mjs").then((n) => n.s).then(
       (m) => m.default ?? m
     );
   }
