aedfd2329d9e8a2d44429d1fd0cfcc1262c87de4
DESKTOP-J77BNUC\Admin <nevikup@gmail.com>
2026-07-14 18:24:41 +0700

    deploy ads route

--- server/_ssr/index.mjs
+++ server/_ssr/index.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 let serverEntryPromise;
 async function getServerEntry() {
   if (!serverEntryPromise) {
-    serverEntryPromise = import("./server-BdZ_lyIR.mjs").then((n) => n.s).then(
+    serverEntryPromise = import("./server-B5sCLhvk.mjs").then((n) => n.s).then(
       (m) => m.default ?? m
     );
   }
