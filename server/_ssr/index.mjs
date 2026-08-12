8213e8b916d15d745fa4f3180ce7d074ecc7c684
DESKTOP-J77BNUC\Admin <nevikup@gmail.com>
2026-08-12 21:05:31 +0700

    deploy contact

--- server/_ssr/index.mjs
+++ server/_ssr/index.mjs
@@ -53,7 +53,7 @@ function renderErrorPage() {
 let serverEntryPromise;
 async function getServerEntry() {
   if (!serverEntryPromise) {
-    serverEntryPromise = import("./server-jfwlcESq.mjs").then((n) => n.s).then(
+    serverEntryPromise = import("./server-YzTQZj6A.mjs").then((n) => n.s).then(
       (m) => m.default ?? m
     );
   }
